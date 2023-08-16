633f6f85bcda0e09224357fc8164206ac172a1b6
Imre Racz <imre@riltech.co>
2023-08-16 16:08:25 +0200

    feat(counting): now counts and waits for 3 seconds before start

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -13,11 +14,17 @@ public class GameManager : MonoBehaviour
     [SerializeField] private bool isRandom = true;
     [SerializeField] private SpawnController spawnController;
     private GameObject _birdPrefab;
+    private List<GameObject> _countPrefabs;
     private Coroutine _spawning;
     
     private void Awake()
     {
         _birdPrefab = Resources.Load<GameObject>("Prefabs/Bird");
+        _countPrefabs = new List<GameObject>() {
+            Resources.Load<GameObject>("Prefabs/3"),
+            Resources.Load<GameObject>("Prefabs/2"),
+            Resources.Load<GameObject>("Prefabs/1"),
+        };
     }
 
     private void Start()
@@ -28,10 +35,14 @@ public class GameManager : MonoBehaviour
     IEnumerator DelayStart()
     {
         bool isDelayed = false;
-        if (!isDelayed)
-        {
+        if (!isDelayed) {
             isDelayed = true;
-            yield return new WaitForSeconds(1);
+            foreach (var counterPrefab in _countPrefabs) {
+                var numberPrefab = Instantiate(counterPrefab, new Vector3(0, 0, -1), quaternion.identity);
+                yield return new WaitForSeconds(1);
+                numberPrefab.SetActive(false);
+                Destroy(numberPrefab, 5);
+            }
         }
         var bird = Instantiate(_birdPrefab, Vector3.zero, quaternion.identity);
         bird.GetComponent<BirdController>().die.AddListener(GameOver);
